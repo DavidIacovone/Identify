d730a5776d96d81a1273b1b6e1f6c693b5b249be
Davide Iacovone <Iacovone611@gmail.com>
2022-05-30 21:58:21 +0200

    Added auth route

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+//Import routes
+const authRoute = require('./routes/auth');
+
+//Middleware
+app.use('/users/', authRoute);
+
 app.listen(3000);
