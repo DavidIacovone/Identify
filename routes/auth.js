3918b2c76aa32c9832bfbdbc8615ff648e4f4e08
Davide Iacovone <Iacovone611@gmail.com>
2022-06-01 00:56:21 +0200

    Register endpoint implemented

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
+const User = require('../models/User');
+
+router.post('/register', async (req, res)=>{
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        savedUser = await user.save();
+        res.send(savedUser);
+    } catch (error) {
+        res.statusCode(400).send(error);
+    }
+});
 
 module.exports = router;
