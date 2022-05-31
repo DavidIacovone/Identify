fff4b41d6d2339646f4824f06be51d9faaa8aa9b
Davide Iacovone <Iacovone611@gmail.com>
2022-05-31 17:51:48 +0200

    database connection implemented

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongo = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
+
+dotenv.config();
+
+//db connection
+mongo.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, ()=>{console.log("connected to db")})
 
 //Import routes
 const authRoute = require('./routes/auth');
