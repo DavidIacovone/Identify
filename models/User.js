bd0682bb17f4306571f0e49b64325793741704f3
Davide Iacovone <Iacovone611@gmail.com>
2022-05-31 18:16:14 +0200

    User model created

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongo = require('mongoose');
+const { required } = require('nodemon/lib/config');
+
+const userSchema = new mongo.Schema({
+    name: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 100,
+        min: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+        min: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongo.model('User', userSchema);
