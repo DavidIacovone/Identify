440f5adda85c780da32721d9208d7c2b521db892
Davide Iacovone <Iacovone611@gmail.com>
2022-06-09 13:28:44 +0200

    Updated the register endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -4,17 +4,25 @@ const { registerSchema } = require('../services/validation');
 
 router.post('/register', async (req, res, next)=>{
 
-    try {
+    try { 
+        
+        //validate the data from request body
+        const result = await registerSchema.validateAsync(req.body);
+
+        //check if the user already exists by email
+        const emailExists = await User.findOne({email: req.body.email});
+        if (emailExists) return res.status(400).send("An account connected to this email already exists");
+
+        //create the new user
         const user = new User({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password
         });
 
-        const result = await registerSchema.validateAsync(req.body);
-
+        //save the user to the database
         savedUser = await user.save();
-        res.send(savedUser)
+        res.status(201).send(savedUser)
 
     } catch (error) {
         if(error.isJoi === true) error.status = 422;
